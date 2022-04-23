6f890d170012624fefa14bdbd6906e784f2bba0d
chiconunes <fcn_jr@hotmail.com>
2022-04-22 21:56:26 -0300

    Aula 0406 e 0407 - Trabalhando Get e Set

--- User.js
+++ User.js
@@ -1,19 +1,62 @@
-export default class User{
+export default class User {
     #nome
+    #sobrenome
     #email
     #nascimento
     #role
     #ativo
-    constructor(nome, email, nascimento, role, ativo = true){
+
+    constructor(nome, sobrenome, email, nascimento, role, ativo = true) {
         this.#nome = nome
+        this.#sobrenome = sobrenome
         this.#email = email
         this.#nascimento = nascimento
         this.#role = role || 'estudante'
         this.#ativo = ativo
     }
-    exibirInfos(){
+
+    get nome() {
+        return `${this.#nome} ${this.#sobrenome}`
+    }
+     
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    set nome(novoNome){
+        if (novoNome === '') {
+            throw new Error('formato não válido')            
+        }
+        let [nome, ...sobrenome] = novoNome.split(" ")
+        sobrenome = sobrenome.join(" ")
+        //console.log(`nome ${nome}`,`sobrenome ${sobrenome}`)
+        this.#nome = nome
+        this.#sobrenome = sobrenome
+    }
+
+    set nascimento(novoNascimento){
+        this.#nascimento = novoNascimento
+    }
+    
+    set role(novoRole){
+        this.#role = novoRole
+    }
+
+    set ativo(novoAtivo){
+        this.#ativo = novoAtivo
+    }
+
+    exibirInfos() {
         return `${this.#nome},${this.#email}`
     }
 }
 
- 
--- index.js
+++ index.js
@@ -7,3 +7,7 @@ import chalk from "chalk";
 const novoUser = new User("Mariana","m@m.com",'2021-01-01')
 console.log(chalk.red(novoUser.exibirInfos()))
    
+const novoAdmin = new Admin('Rodrigo','r@r.com','2021-0101')
+console.log(chalk.blue(novoAdmin.nome))
+novoAdmin.nome = 'Francisco Cardoso Nunes'
+console.log(chalk.blue(novoAdmin.nome))
